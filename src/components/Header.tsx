78c67bdae8989f5cf19efeef4ca98a21e1d6d404
Carolina Parisi <carolinaparisi5@gmail.com>
2024-06-25 16:24:38 +0100

    feat: create Home and Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+	return (
+		<header className="bg-black h-20 flex">
+			<button className="text-white pl-8">HOME</button>
+			<button className="text-gray pl-8">BLOGS</button>
+			<button className="text-gray pl-8">ARTICLES</button>
+			<button className="text-gray pl-8">REPORTS</button>
+		</header>
+	);
+}
