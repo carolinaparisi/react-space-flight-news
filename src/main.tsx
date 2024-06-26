8aae3be8230de1c203048f055a4e4ba28ba5cd37
Carolina Parisi <carolinaparisi5@gmail.com>
2024-06-26 17:56:29 +0100

    feat: install react-router-dom, implement React routing and create Articles, Reports and Blogs pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import Blogs from "./pages/Blogs.tsx";
+import Articles from "./pages/Articles.tsx";
+import Reports from "./pages/Reports.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <h1>THIS PAGE IS NOT FOUND!</h1>,
+	},
+	{
+		path: "/blogs",
+		element: <Blogs />,
+	},
+	{
+		path: "/articles",
+		element: <Articles />,
+	},
+	{
+		path: "/reports",
+		element: <Reports />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		{/*This is the entry point of the application: */}
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
---
+++ src/pages/Articles.tsx
@@ -0,0 +1,3 @@
+export default function Articles() {
+	return <h1>ARTICLES PAGE!</h1>;
+}
