2e93c09c2cdea414a712296f08025ef1f7465b12
Carolina Parisi <carolinaparisi5@gmail.com>
2024-06-28 01:26:57 +0100

    refactor: update Banner and MainContent component in order to be reusable in all pages

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,49 @@
 import banner from "../assets/space-ship-banner.png";
 
-export default function Banner() {
+interface BannerProps {
+	imageUrl?: string;
+	title?: string;
+	summary?: string;
+	url?: string;
+}
+
+export default function Banner({ imageUrl, title, summary, url }: BannerProps) {
 	return (
 		<div className="relative">
-			<img src={banner} className="opacity-20" />
-			<h1 className="text-white absolute inset-y-36 left-8 text-5xl font-bold">
-				<span className="text-blue">React </span>
-				Space News API
-			</h1>
+			<img
+				src={imageUrl ? imageUrl : banner}
+				className="opacity-20 w-full h-96 object-cover"
+			/>
+			<div className=" text-white absolute inset-y-0 left-0 px-8 h-full flex flex-col justify-center w-full gap-2">
+				<h1 className=" text-5xl font-bold">
+					{title ? (
+						title
+					) : (
+						<>
+							<span className="text-blue">React </span>
+							Space News API
+						</>
+					)}
+				</h1>
+				{summary ? (
+					<div className=" overflow-hidden whitespace-nowrap text-ellipsis">
+						{summary}
+					</div>
+				) : (
+					""
+				)}
+				{url ? (
+					<button
+						className="flex mt-4"
+						onClick={() => {
+							console.log(url);
+						}}>
+						READ BLOG
+					</button>
+				) : (
+					""
+				)}
+			</div>
 		</div>
 	);
 }
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,13 +1,36 @@
-export default function MainContent() {
+import { Blog } from "../pages/Blogs";
+
+interface MainContentProps {
+	blogs?: Blog[];
+}
+
+export default function MainContent({ blogs }: MainContentProps) {
 	return (
-		<main className="bg-black text-white min-h-64 flex items-center pl-8 text-justify">
-			<div className="w-3/5">
-				This website provides information on space launches and missions, making
-				it a must-have for spaceflight enthusiasts. Here, you will find blogs,
-				articles, and even reports on these events. The goal is to inform and
-				update those interested in this niche, offering an expository take on
-				the field.
-			</div>
+		<main className="bg-black text-white min-h-64 flex items-center p-8 text-justify">
+			{blogs ? (
+				<div className="border border-blue w-full">
+					{blogs.map((blog) => {
+						return (
+							<div className="border-b border-white" key={blog.id}>
+								<div>{blog.title}</div>
+								<div>{blog.summary}</div>
+								<div>
+									Published by {blog.news_site},{" "}
+									{blog.published_at.split("T")[0]}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			) : (
+				<div className="w-3/5">
+					This website provides information on space launches and missions,
+					making it a must-have for spaceflight enthusiasts. Here, you will find
+					blogs, articles, and even reports on these events. The goal is to
+					inform and update those interested in this niche, offering an
+					expository take on the field.
+				</div>
+			)}
 		</main>
 	);
 }
